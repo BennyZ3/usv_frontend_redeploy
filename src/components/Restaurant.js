1480e3a1072bd8152bc1e401a9f9577654b2b61b
BennyZ3 <bennyz3@hotmail.com>
2022-06-29 12:27:40 -0400

    added tab icon and tab name

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -21,12 +21,14 @@ const Restaurant = (props) => {
     nav(`/restaurants/${id}`);
   };
   return (
-    <div className="RestaurantCard" onClick={handleClick}>
-      <img src="https://www.ipcc.ch/site/assets/uploads/sites/3/2019/10/img-placeholder.png" />
-      <h2>{name}</h2>
-      <p>{cuisine}</p>
-      <p>{location}</p>
-      <p>{price}</p>
+    <div className="RestaurantCard">
+      <div onClick={handleClick}>
+        <img src="https://www.ipcc.ch/site/assets/uploads/sites/3/2019/10/img-placeholder.png" />
+        <h2>{name}</h2>
+        <p>{cuisine}</p>
+        <p>{location}</p>
+        <p>{price}</p>
+      </div>
       {/* <p>{description}</p> */}
       {/* <p>{price}</p>
         <p>{price}</p> */}
