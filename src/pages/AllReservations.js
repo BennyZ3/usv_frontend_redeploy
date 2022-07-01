481bfac5e5269d7b82508f54d45e26b5d0b54794
BennyZ3 <bennyz3@hotmail.com>
2022-06-30 21:11:16 -0400

    added view all reservations and reservation details page

---
+++ src/pages/AllReservations.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AllReservations = () => {
+  const [reservations, setReservations] = useState([]);
+  const API = process.env.REACT_APP_API_URL;
+  const nav = useNavigate();
+  useEffect(() => {
+    axios
+      .get(`${API}/api/reservations`)
+      .then((res) => {
+        setReservations(res.data.reservations);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  const handleClick = (event) => {
+    nav(`/reservations/${event.target.id}`);
+  };
+  return (
+    <div>
+      <h1>All Reservations</h1>
+      {reservations.map((res) => {
+        return (
+          <div onClick={handleClick} id={res.id}>
+            {res.firstName} {res.lastName} {res.time}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AllReservations;
